ea28d53c47547777e54f23d9e2e7dfab95c20ac2
Konrad <konrad_here@hotmail.com>
2017-09-27 20:59:49 -0700

    add scale control and our 4 tile base layers

--- src/components/MapContainer/MapContainer.jsx
+++ src/components/MapContainer/MapContainer.jsx
@@ -7,6 +7,7 @@ import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { Map,
   LayersControl,
+  ScaleControl,
   ZoomControl,
   TileLayer,
   GeoJSON,
@@ -73,25 +74,35 @@ export default class MapContainer extends Component {
         style={{ width: '100%' }}
 
         onViewportChanged={clearBounds}
-
-        zoomControl={false}
-        attributionControl={false}
       >
+        <ScaleControl position="bottomright" />
         <ZoomControl position="bottomright" />
-
         <LayersControl position='topright'>
           <LayersControl.BaseLayer name='OpenStreetMap' checked>
             <TileLayer
-              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+              url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name='OpenStreetMap.BlackAndWhite'>
+          <LayersControl.BaseLayer name='Humanitarian OpenStreetMap'>
             <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url='http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'
+              url='http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+              attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
             />
           </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='ESRI World Imagery'>
+            <TileLayer
+              url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+              attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='ESRI World Topographic Map'>
+            <TileLayer
+              url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+              attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+            />
+          </LayersControl.BaseLayer>
+
           { templates.map(template =>
             <LayersControl.Overlay name={template.name} checked>
               <GeoJSON data={template.mapItems} />
